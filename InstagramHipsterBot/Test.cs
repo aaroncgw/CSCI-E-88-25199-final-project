05748ec2c1189b4bbfd43ddb976b6a14e26d88f2
Oleg Demchenko <oldemche@microsoft.com>
2018-05-08 11:34:08 -0400

    Add more comments

--- InstagramHipsterBot/Test.cs
+++ InstagramHipsterBot/Test.cs
@@ -21,7 +21,10 @@ namespace InstagramHipsterBot
     [TestFixture]
     public class Test
     {
+        // AWS cluster master node url
         const string Ec2MasterUrl = "http://ec2-18-188-248-171.us-east-2.compute.amazonaws.com/";
+
+        // Custom Vision API url
         const string CustomVisionUrl = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/e74dd823-4617-4ff2-94dd-6360ce255414/image?iterationId=f8ef8c82-6560-4df9-a6b2-b5c02b951d50";
 
         string fisherHipsterDeviceIdentifier = "57ce11c580f8ffdf9a10967fbcc682e63c29d0b5";
@@ -43,6 +46,7 @@ namespace InstagramHipsterBot
         [SetUp]
         public void Setup ()
         {
+            // Configure device ID and device IP
             string deviceUDID = string.Empty;
             string deviceIP = string.Empty;
 
@@ -54,6 +58,7 @@ namespace InstagramHipsterBot
                 deviceIP = coffeeHipsterDeviceIP;
             }
 
+            // Install and connect to the iOS app running on the phone
             app = ConfigureApp.iOS.
                 DeviceIdentifier(deviceUDID).
                 DeviceIp(deviceIP).
@@ -66,17 +71,20 @@ namespace InstagramHipsterBot
         [TearDown]
         public void TearDown ()
         {
+            // Destroy the app
             app = null;
         }
 
         [Test]
         public void LikeLoop ()
         {
+            // Run infinite loop
             while (true) {
                 var all = app.Query(q => q.Class("WKWebView").Css("*"));
 
                 var hashtags = new List<string> ();
 
+                // Search for hashtag
                 foreach (var element in all) {
                     if (!string.IsNullOrEmpty(element.TextContent) && element.Class == "_ezgzd") {
                         var regex = new Regex(@"(?<=#)\w+");
@@ -87,18 +95,23 @@ namespace InstagramHipsterBot
                     }
                 }
 
+                // Search for the like button
                 foreach (var element in all) {
                     if (element.Class.Contains("coreSpriteHeartOpen")) {
+                        // Tap the like button
                         app.TapCoordinates(element.Rect.CenterX, element.Rect.CenterY);
 
+                        // Capture a screenshot
                         var imageFileInfo = app.Screenshot(DateTime.Now.ToString());
 
                         CustomVisionResponse imagePrediction = null;
 
                         try {
+                            // Analyze the image with Microsoft Custom Vision
                             var predictionRequestTask = MakePredictionRequest(imageFileInfo.FullName);
                             predictionRequestTask.Wait();
 
+                            // Save predictions
                             imagePrediction = predictionRequestTask.Result;
                         } catch(Exception ex) {
                             Console.WriteLine($"Failed to get image ML details: {ex.Message}");
@@ -107,6 +120,7 @@ namespace InstagramHipsterBot
                         }
 
                         try {
+                            // Report results: hashtags + ML predictions + metadata to AWS
                             ReportLikeToAWS (imagePrediction, hashtags).Wait();
                         } catch(Exception ex) {
                             Console.WriteLine($"Failed to report like to AWS: {ex.Message}");
